e0c575e6cf13487b3daac6061c06ae904cbdc5a9
davidyano600 <godsentryan@gmail.com>
2025-07-18 08:40:36 +0100

    first commit

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,36 @@
+//  let name:string;
+//  let age:number | string;
+//  let isPaid:boolean;
+//  let hobbies:string[];
+//  let role: [number, string];
+
+
+//  type Person = {
+//   name:string;
+//   age:number;
+//  }
+//  let person:Person ={
+//   name:"Dave",
+//   age: 24
+//  }
+
+//  function printname(name:string) {
+//   console.log(name)
+//  }
+//  printname("dave")
+// interface Person{
+// name:string;
+// age:number;
+// }
+// interface guy extends Person{
+//   profession:string
+// }
+
+const App: React.FC = () => {
+
+  return (
+    <div>Hello world</div>
+  )
+}
+
+export default App
